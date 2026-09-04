224f5ff96efe518359d65dd0aac6c9daa947a837
ammaroussema <ammaroussema@users.noreply.github.com>
2026-09-04 15:31:31 +0100

    version bump

--- src/main.rs
+++ src/main.rs
@@ -895,7 +895,7 @@ const MEGAMAN_COLORS: UiColors = UiColors {
     dip_on_fill: 0xFF00CCFF,
 };
 
-const APP_VERSION: &str = "1.6.4";
+const APP_VERSION: &str = "1.6.5";
 
 fn strip_version_prefix(s: &str) -> &str {
     s.trim_start_matches(|c: char| c.is_ascii_alphabetic())
@@ -1414,7 +1414,7 @@ fn main() {
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
-        .with_title("AccuNES 1.6.4")
+        .with_title("AccuNES 1.6.5")
         .with_inner_size(winit::dpi::PhysicalSize::new(window_width, window_height))
         .with_window_icon(Some(icon))
         .build(&event_loop)
@@ -7203,9 +7203,9 @@ fn main() {
                         } else if lower.ends_with(".fds") || lower.ends_with(".qd") || lower.ends_with(".studybox") || lower.ends_with(".study") {
                             filename.truncate(filename.len() - 4);
                         }
-                        format!("AccuNES 1.6.4: {}", filename)
+                        format!("AccuNES 1.6.5: {}", filename)
                     } else {
-                        "AccuNES 1.6.4".to_string()
+                        "AccuNES 1.6.5".to_string()
                     };
                     let title = if *fps_mode_clone.borrow() == config::FpsMode::Window {
                         format!("{} - {} FPS", base_title, fps)
@@ -7629,7 +7629,7 @@ fn main() {
                         "AccuNES",
                         "Accurate NES/Famicom Emulator",
                         "Created by: Oussema Ammar",
-                        "Version: 1.6.4",
+                        "Version: 1.6.5",
                     ];
                     let line_spacing = (20.0 * scale).round() as usize;
                     let icon_offset = if ms.about_icon_data.is_some() { (50.0 * scale).round() as usize } else { 0 };
